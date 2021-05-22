089f3c15b1f52cc2b6c88696df14cea26704e8fa
Sasha <sasha.zalesskiy@mail.ru>
2021-05-22 20:57:47 +0300

    Task 1.5

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,86 @@
 package com.es.phoneshop.model.product;
 
+import com.es.phoneshop.data.DataGenerator;
+import com.es.phoneshop.model.product.exception.ProductNotFoundException;
+
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+
+    private List<Product> productList;
+
+    private long newId;
+
+    private static final ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
+
+    public ArrayListProductDao() {
+        this.productList = DataGenerator.generateSampleProducts();
+        if (productList.size() != 0) {
+            newId = productList.get(productList.size() - 1).getId();
+        }
+    }
+
     @Override
-    public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+    public Product getProduct(Long id) throws ProductNotFoundException {
+        lock.readLock().lock();
+        try {
+            return productList.stream()
+                    .filter(product -> id.equals(product.getId()))
+                    .findAny()
+                    .orElseThrow(ProductNotFoundException::new);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+        lock.readLock().lock();
+        try {
+            return productList.stream()
+                    .filter(this::isPriceNotNull)
+                    .filter(this::isProductInStock)
+                    .collect(Collectors.toList());
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     @Override
     public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+        lock.writeLock().lock();
+        try {
+            if (product.getId() != null) {
+                productList.set(productList.indexOf(getProduct(product.getId())), product);
+                return;
+            }
+            product.setId(++newId);
+            productList.add(product);
+        } catch (ProductNotFoundException ignored) {
+
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     @Override
     public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+        lock.writeLock().lock();
+        try {
+            productList.remove(getProduct(id));
+        } catch (ProductNotFoundException ignored) {
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    private boolean isProductInStock(Product product) {
+        return product.getStock() > 0;
+    }
+
+    private boolean isPriceNotNull(Product product) {
+        return product.getPrice() != null;
     }
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -1,12 +1,15 @@
 package com.es.phoneshop.model.product;
 
+import com.es.phoneshop.model.product.exception.ProductNotFoundException;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
 
-public class ArrayListProductDaoTest
-{
+import static org.junit.Assert.*;
+
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
 
     @Before
@@ -16,6 +19,57 @@ public class ArrayListProductDaoTest
 
     @Test
     public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+        assertFalse(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testSaveNewProduct() throws ProductNotFoundException {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+
+        assertTrue(product.getId() > 0);
+
+        Product check = productDao.getProduct(product.getId());
+
+        assertNotNull(check);
+        assertEquals("sgs", check.getCode());
+    }
+
+    @Test
+    public void testFindProductWithZeroStock() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+
+        assertFalse(productDao.findProducts().contains(product));
+    }
+
+    @Test
+    public void testFindProductWithNullPrice() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("sgs", "Samsung Galaxy S", null, usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+
+        assertFalse(productDao.findProducts().contains(product));
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        productDao.delete(product.getId());
+
+        assertFalse(productDao.findProducts().contains(product));
+    }
+
+    @Test
+    public void testGetProduct() throws ProductNotFoundException {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+
+        assertEquals(product.getId(), productDao.getProduct(product.getId()).getId());
     }
 }
